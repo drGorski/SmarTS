6d8b201f13dcd77f657673430b5bc21c61e668f5
drGorski <czasnagdynie@gmail.com>
2024-01-21 13:24:25 +0100

    Run class

--- src/pl/gdansk/ug/Run.java
+++ src/pl/gdansk/ug/Run.java
@@ -9,9 +9,12 @@ import pl.gdansk.ug.testSC.TestSendEnergySC;
 
 public class Run {
     public static void main(String[] args){
+        // declare variables
+        AbstractSmartContract sC;
+        AbstractTestSC testSC;
         // run tests for SendEnergySC
-        AbstractSmartContract sC = new SendEnergySC();
-        AbstractTestSC testSC = new TestSendEnergySC();
+        sC = new SendEnergySC();
+        testSC = new TestSendEnergySC();
         testSC.runTestSuite(sC);
         // run tests for InsuranceTransactionSC
         sC = new InsuranceConclusionSC();
