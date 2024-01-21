aa8bcef879d63b3cff58d1ff16723bb0db3d1ec3
drGorski <czasnagdynie@gmail.com>
2024-01-21 13:31:46 +0100

    RunTestSuite class

--- src/pl/gdansk/ug/RunTestSuite.java
+++ src/pl/gdansk/ug/RunTestSuite.java
@@ -7,16 +7,16 @@ import pl.gdansk.ug.reducT.AbstractTestSC;
 import pl.gdansk.ug.testSC.TestInsuranceConclusionSC;
 import pl.gdansk.ug.testSC.TestSendEnergySC;
 
-public class Run {
+public class RunTestSuite {
     public static void main(String[] args){
         // declare variables
         AbstractSmartContract sC;
         AbstractTestSC testSC;
-        // run tests for SendEnergySC
+        // run test suite for SendEnergySC
         sC = new SendEnergySC();
         testSC = new TestSendEnergySC();
         testSC.runTestSuite(sC);
-        // run tests for InsuranceTransactionSC
+        // run test suite InsuranceTransactionSC
         sC = new InsuranceConclusionSC();
         testSC = new TestInsuranceConclusionSC();
         testSC.runTestSuite(sC);
