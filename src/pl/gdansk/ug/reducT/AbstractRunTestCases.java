a330a3c429a551ea238017b493fcb30231034f2d
drGorski <czasnagdynie@gmail.com>
2024-01-21 18:46:34 +0100

    AbstractRunTestCases class

--- src/pl/gdansk/ug/reducT/AbstractRunTestCases.java
+++ src/pl/gdansk/ug/reducT/AbstractRunTestCases.java
@@ -5,12 +5,12 @@ import pl.gdansk.ug.abstractSC.AbstractTransaction;
 
 import java.util.ArrayList;
 public class AbstractRunTestCases {
-    public static void runTest(AbstractSmartContract sC, AbstractTransaction tR, int trNumber, boolean expectedResult){
+    private static void runTest(AbstractSmartContract sC, AbstractTransaction tR, int trNumber, boolean expectedResult){
         boolean result = sC.checkSC(tR);
         boolean correct = result == expectedResult;
         System.out.println("Test no: " + (trNumber + 1) + ", test result: " + ((correct)?"PASS":"FAIL"));
     }
-    public static void runTestSuite(AbstractSmartContract sC, ArrayList<AbstractTransaction> transactions){
+    static void runTestSuite(AbstractSmartContract sC, ArrayList<AbstractTransaction> transactions){
         System.out.println("Smart contract class: " + sC.getClass());
         for (int i = 0; i < transactions.size(); i++){
             runTest(sC, transactions.get(i),i, i == 0);
