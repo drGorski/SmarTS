5fa673edd0876163c97ce847cc35bbd741ab6c9e
drGorski <czasnagdynie@gmail.com>
2024-01-22 11:15:31 +0100

    TestInsuranceConclusionSC class InsuranceConclusionSC class

--- src/pl/gdansk/ug/concreteSC/InsuranceConclusionSC.java
+++ src/pl/gdansk/ug/concreteSC/InsuranceConclusionSC.java
@@ -1,15 +1,16 @@
 package pl.gdansk.ug.concreteSC;
 
 import pl.gdansk.ug.abstractSC.AbstractSmartContract;
-
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class InsuranceConclusionSC extends AbstractSmartContract {
     public InsuranceConclusionSC(){
         rulesList = Arrays.asList(
+                t -> !((InsuranceTransaction) t).getDateFrom().isBefore(LocalDate.now()),
                 t -> ((InsuranceTransaction) t).getDateFrom().isBefore(((InsuranceTransaction) t).getDateTo()),
-                t -> ((InsuranceTransaction) t).getInsurancePremium() > 0,
                 t -> ((InsuranceTransaction) t).getInsuranceAmount() > 0,
+                t -> ((InsuranceTransaction) t).getInsurancePremium() > 0,
                 t -> ((InsuranceTransaction) t).getInsurancePremium() < ((InsuranceTransaction) t).getInsuranceAmount());
     }
 }
--- src/pl/gdansk/ug/testSC/TestInsuranceConclusionSC.java
+++ src/pl/gdansk/ug/testSC/TestInsuranceConclusionSC.java
@@ -6,10 +6,11 @@ import java.time.LocalDate;
 public class TestInsuranceConclusionSC extends AbstractTestSC {
     public void initiateTransactions(){
         transactions.clear();
-        transactions.add(new InsuranceTransaction(LocalDate.of(2024,1,20), LocalDate.of(2025,1,19),250.0,10000.0));
-        transactions.add(new InsuranceTransaction(LocalDate.of(2025,1,20), LocalDate.of(2025,1,19),250.0,10000.0));
-        transactions.add(new InsuranceTransaction(LocalDate.of(2024,1,20), LocalDate.of(2025,1,19),250.0,0.0));
-        transactions.add(new InsuranceTransaction(LocalDate.of(2024,1,20), LocalDate.of(2025,1,19),0.0,10000.0));
-        transactions.add(new InsuranceTransaction(LocalDate.of(2024,1,20), LocalDate.of(2025,1,19),250.0,100.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now(), LocalDate.now().plusYears(1),250.0,10000.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now().minusDays(1), LocalDate.now().plusYears(1),250.0,10000.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now(), LocalDate.now().minusMonths(1),250.0,10000.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now(), LocalDate.now().plusYears(1),250.0,0.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now(), LocalDate.now().plusYears(1),0.0,10000.0));
+        transactions.add(new InsuranceTransaction(LocalDate.now(), LocalDate.now().plusYears(1),250.0,100.0));
     }
 }
